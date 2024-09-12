f69a0fa30735a658d6ca6aa543f76b20d734700d
saitejamandadi99 <saitejamandadi99>
2024-09-12 07:24:18 +0000

    initial code

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,10 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="userInfoContainer">
+    <img />
+    <h1>Wade Warrren </h1>
+    <p>Software Developer at UK</p>
+  </div>
+)
