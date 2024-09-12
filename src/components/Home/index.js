4d45b0ac13fb66ac034dd6944311b32b048403c4
saitejamandadi99 <saitejamandadi99>
2024-09-12 07:36:08 +0000

    added user info

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,5 @@
+import UserInfo from '../UserInfo'
+
 import './index.css'
 
 const blogsList = [
@@ -35,7 +37,10 @@ const blogsList = [
   },
 ]
 const Home = () => (
-  <div className="home-container">Render UserInfo and BlogList</div>
+  <div className="home-container">
+    Render UserInfo and BlogList
+    <UserInfo />
+  </div>
 )
 
 export default Home
--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -3,8 +3,13 @@ import './index.css'
 
 const UserInfo = () => (
   <div className="userInfoContainer">
-    <img />
-    <h1>Wade Warrren </h1>
-    <p>Software Developer at UK</p>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="userImage"
+    />
+    <h1 className="userName">Wade Warrren </h1>
+    <p className="userDetails">Software Developer at UK</p>
   </div>
 )
+export default UserInfo
